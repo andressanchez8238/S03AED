f36a8279a2e46073038eec785813d60615c3f9f9
andressanchez8238 <andres.sanchez@tecsup.edu.pe>
2026-04-01 12:23:12 -0500

    intento

---
+++ Assets/Scripts/NodeActions.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+using Sirenix.OdinInspector;
+using System.Collections.Generic;
+public class NodeActions
+{
+    public enum mover
+    {
+
+    }
+    public enum atacar
+    {
+
+    }
+    public enum defender
+    {
+
+    }
+}
